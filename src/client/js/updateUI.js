b5f7b5a649a4993df1361374a269539a9b580f03
Caitlin Elliott <ceelliott90@gmail.com>
2021-05-18 11:16:21 -0400

    added forecast output js & styles

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -14,7 +14,25 @@ function updateUI(tripState, userCountry, userCity, departDate, returnDate, weat
             console.log(forecast[i].low_temp);
             console.log(forecast[i].weather.icon);
 
-            document.querySelector('.forecast-date').innerHTML = forecast[i].high_temp;
+            const newRow = document.createElement('div');
+            newRow.classList.add('forecast-row');
+            document.querySelector('.forecast').appendChild(newRow)
+
+            const tripDate = document.createElement('div');
+            tripDate.classList.add('forecast-date');
+            tripDate.innerHTML = `${dates[i].getMonth() + 1}/${dates[i].getDate() + 1}`;
+            newRow.appendChild(tripDate);
+
+            const weatherIcon = document.createElement('img');
+            weatherIcon.classList.add('forecast-icon');
+            weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${forecast[i].weather.icon}.png`;
+            // weatherIcon.innerHTML = forecast[i].weather.icon;
+            newRow.appendChild(weatherIcon);
+
+            const weather = document.createElement('div');
+            weather.classList.add('forecast-high');
+            weather.innerHTML = `${forecast[i].high_temp}*F / ${forecast[i].low_temp}*F`;
+            newRow.appendChild(weather);
         }
         else {
             console.log('feet');
@@ -27,12 +45,12 @@ function updateUI(tripState, userCountry, userCity, departDate, returnDate, weat
     const travelSum = document.querySelector('.travel-summary');
     const currentDate = new Date();
     output.classList.add('display-on');
-    h1.innerHTML = `${userCity}, ${tripState}`;
-    h2.innerHTML = `${monthNames[departDate.getMonth()]} ${departDate.getDate()}, ${departDate.getFullYear()} - ${monthNames[returnDate.getMonth()]} ${returnDate.getDate()}, ${returnDate.getFullYear()}`;
+    h1.innerHTML = `${userCity}, ${tripState} `;
+    h2.innerHTML = `${monthNames[departDate.getMonth()]} ${departDate.getDate()}, ${departDate.getFullYear()} - ${monthNames[returnDate.getMonth()]} ${returnDate.getDate()}, ${returnDate.getFullYear()} `;
     travelSum.innerHTML =
         `I can see that you are traveling to ${userCity} from ${h2.innerHTML}.
-        Your trip is ${returnDate.getDate() - departDate.getDate() + 1} days and ${returnDate.getDate() - departDate.getDate()} nights long. 
-        Your trip begins in ${departDate.getDate() - currentDate.getDate()} days.`
+            Your trip is ${returnDate.getDate() - departDate.getDate() + 1} days and ${returnDate.getDate() - departDate.getDate()} nights long.
+            Your trip begins in ${departDate.getDate() - currentDate.getDate()} days.`
 }
 
 export { updateUI }
