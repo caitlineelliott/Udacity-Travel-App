2a565c07e0610c97c0142048b4361e23f8cb0ab4
Caitlin Elliott <ceelliott90@gmail.com>
2021-05-18 11:42:03 -0400

    changed style of .travel-summary

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -44,15 +44,15 @@ function updateUI(tripState, userCountry, userCity, departDate, returnDate, weat
     const h1 = document.querySelector('.title');
     const h2 = document.querySelector('.subtitle');
     const output = document.querySelector('.output');
-    const travelSum = document.querySelector('.travel-summary');
+    // const travelSum = document.querySelector('.travel-summary');
     const currentDate = new Date();
     output.classList.add('display-on');
     h1.innerHTML = `${userCity}, ${tripState} `;
     h2.innerHTML = '';
-    travelSum.innerHTML =
-        `I can see that you are traveling to ${userCity} from ${monthNames[departDate.getMonth()]} ${departDate.getDate()}, ${departDate.getFullYear()} - ${monthNames[returnDate.getMonth()]} ${returnDate.getDate()}, ${returnDate.getFullYear()}.
-            Your trip is ${returnDate.getDate() - departDate.getDate() + 1} days and ${returnDate.getDate() - departDate.getDate()} nights long.
-            Your trip begins in ${departDate.getDate() - currentDate.getDate()} days.`
+    // travelSum.innerHTML =
+    //     `I can see that you are traveling to ${userCity} from ${monthNames[departDate.getMonth()]} ${departDate.getDate()}, ${departDate.getFullYear()} - ${monthNames[returnDate.getMonth()]} ${returnDate.getDate()}, ${returnDate.getFullYear()}.
+    //         Your trip is ${returnDate.getDate() - departDate.getDate() + 1} days and ${returnDate.getDate() - departDate.getDate()} nights long.
+    //         Your trip begins in ${departDate.getDate() - currentDate.getDate()} days.`
 }
 
 export { updateUI }
