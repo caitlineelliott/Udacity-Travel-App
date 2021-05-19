0c787786ec0d2e277c5fb145eb39bb0b39470551
Caitlin Elliott <ceelliott90@gmail.com>
2021-05-19 13:20:42 -0400

    refined packing list item styles

--- src/client/js/addPackingItem.js
+++ src/client/js/addPackingItem.js
@@ -9,22 +9,28 @@ function addPackingItem(event) {
 
     let newItemValue = document.createElement('div');
     newItemValue.innerHTML = document.querySelector('.packing-list-item').value;
+    newItemValue.classList.add('packing-item-row-segment')
     newItemRow.appendChild(newItemValue);
 
     let newItemCategory = document.createElement('div');
     newItemCategory.innerHTML = document.querySelector('.packing-item-category').value;
+    newItemCategory.classList.add('packing-item-row-segment')
+    newItemCategory.classList.add('item-category')
     newItemRow.appendChild(newItemCategory);
 
     let packedFlag = document.createElement('input');
     packedFlag.setAttribute('type', 'checkbox');
+    packedFlag.classList.add('packed-flag')
     let packedFlagLabel = document.createElement('label');
     packedFlagLabel.innerHTML = 'Packed';
+    packedFlagLabel.classList.add('packing-item-row-segment')
     newItemRow.appendChild(packedFlagLabel);
     packedFlagLabel.appendChild(packedFlag);
 
 
     let deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = `<i class="fas fa-times"></i>`;
+    deleteBtn.classList.add('packing-item-row-segment')
     deleteBtn.classList.add('delete-btn');
     newItemRow.appendChild(deleteBtn);
 
