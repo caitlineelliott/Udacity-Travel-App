59dfba2a5f9f60bd267ad734b2937aa43249c150
Caitlin Elliott <ceelliott90@gmail.com>
2021-05-24 15:04:36 -0400

    began adding edit btn function

--- src/client/js/addPackingItem.js
+++ src/client/js/addPackingItem.js
@@ -7,14 +7,17 @@ function addPackingItem(event) {
     let selectOptions = document.querySelectorAll('option'); // options in select
     let packingListOutput = document.querySelector('.packing-list'); // whole packing list (incl. form)
 
-    // CREATES NEW ITEM BLOCK
+    // CREATES NEW ITEM ROW
     let newItemRow = document.createElement('div');
     newItemRow.classList.add('packing-list-row');
 
     // ADDS ITEM TO BLOCK
-    let newItemValue = document.createElement('div');
-    newItemValue.innerHTML = document.querySelector('.packing-list-item').value;
-    newItemValue.classList.add('packing-item-row-segment')
+    let newItemValue = document.createElement('input');
+    newItemValue.setAttribute('type', 'text');
+    newItemValue.readOnly = true;
+    newItemValue.defaultValue = `${document.querySelector('.packing-list-item').value}`;
+    newItemValue.placeholder = `${document.querySelector('.packing-list-item').value}`;
+    newItemValue.classList.add('packing-item-row-segment', 'packing-item');
     newItemRow.appendChild(newItemValue);
 
     // ADDS CATEGORY TO BLOCK
@@ -46,6 +49,10 @@ function addPackingItem(event) {
     editBtn.innerHTML = `<i class="fas fa-edit"></i>`;
     editBtn.classList.add('packing-item-row-segment', 'delete-btn')
     newItemRow.appendChild(editBtn);
+
+    editBtn.addEventListener('click', function () {
+        newItemValue.readOnly = false;
+    })
 
     // ADDS DELETE BTN TO BLOCK
     let deleteBtn = document.createElement('button');
