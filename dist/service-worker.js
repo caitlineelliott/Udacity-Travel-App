385d229e516cf8153a5ef1fc7b94794180e2ee40
Caitlin Elliott <ceelliott90@gmail.com>
2021-09-01 10:59:30 -0400

    refactored packing/todo items, mobile size

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -114,7 +114,7 @@ define("./service-worker.js",['./workbox-903cc191'], function (workbox) { 'use s
     "revision": null
   }, {
     "url": "main.css",
-    "revision": "b4f76940d0bbe61816769e91da17340d"
+    "revision": "0b452f6195bedf4094e20d98ee510da4"
   }, {
     "url": "main.js",
     "revision": "68c74b6737fc977ab18a8f7186142647"
